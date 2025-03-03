8b17661ff5a5b50c323fc3e647b8e489f4d93a57
NirjalGGMU <anirjal08@gmail.com>
2025-03-03 22:34:22 +0545

    Final Commit

---
+++ server.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const authRoutes = require('./authRoutes');
+
+const app = express();
+const port = 3000;
+
+app.use(bodyParser.json());
+app.use('/api/auth', authRoutes);
+
+// ...existing code...
+
+app.listen(port, () => {
+    console.log(`Server running on port ${port}`);
+});
